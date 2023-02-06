4e62297d969248ad5d7ddbecda4e7414bb30787b
LeoAlles <leonardoralles@gmail.com>
2023-02-06 19:45:29 -0300

    adding cors

--- Back/src/main/java/com/playful/chat/controller/RoomController.java
+++ Back/src/main/java/com/playful/chat/controller/RoomController.java
@@ -4,6 +4,7 @@ import com.playful.chat.controller.response.RoomResponse;
 import com.playful.chat.model.Room;
 import com.playful.chat.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class RoomController {
     private RoomService roomService;
 
     @GetMapping("/search")
+    @CrossOrigin(origins = "http://localhost:3000")
     public List<RoomResponse> listRooms() {
         return roomService.list();
     }
