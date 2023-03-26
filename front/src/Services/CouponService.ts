ecc603940fdc07228246dda5ab3bcfe38b34b70a
LeoAlles <leonardoralles@gmail.com>
2023-03-25 21:50:33 -0300

    undoing mock

--- front/src/Services/CouponService.ts
+++ front/src/Services/CouponService.ts
@@ -36,37 +36,37 @@ export const CouponMapper = (response: CouponResponse): Coupon => {
 export default class CouponService {
   private static endpoint = 'http://localhost:8080/api/coupons'
 
-  // public static async searchAll() {
-  //   const response = await axios.get(this.endpoint)
+  public static async searchAll() {
+    const response = await axios.get(this.endpoint)
 
-  //   return response.data.map(CouponMapper)
-  // }
-
-  public static searchAll(): Promise<Coupon[]> {
-    const user = new User(
-      1, // id
-      "John Doe", // name
-      "johndoe@example.com", // email
-      "password123", // password
-      moment("1990-01-01") // birthDate
-    );
-
-    return new Promise((resolve) => {
-      const user = new User(
-        1, // id
-        "John Doe", // name
-        "johndoe@example.com", // email
-        "password123", // password
-        moment("1990-01-01") // birthDate
-      );
-      const coupons: Coupon[] = [
-        new Coupon(1, "Store A", "ABC123", user, null),
-        new Coupon(2, "Store B", "DEF456", user, null),
-        new Coupon(3, "Store C", "GHI789", user, null),
-      ];
-      resolve(coupons);
-    });
+    return response.data.map(CouponMapper)
   }
+
+  // public static searchAll(): Promise<Coupon[]> {
+  //   const user = new User(
+  //     1, // id
+  //     "John Doe", // name
+  //     "johndoe@example.com", // email
+  //     "password123", // password
+  //     moment("1990-01-01") // birthDate
+  //   );
+
+  //   return new Promise((resolve) => {
+  //     const user = new User(
+  //       1, // id
+  //       "John Doe", // name
+  //       "johndoe@example.com", // email
+  //       "password123", // password
+  //       moment("1990-01-01") // birthDate
+  //     );
+  //     const coupons: Coupon[] = [
+  //       new Coupon(1, "Store A", "ABC123", user, null),
+  //       new Coupon(2, "Store B", "DEF456", user, null),
+  //       new Coupon(3, "Store C", "GHI789", user, null),
+  //     ];
+  //     resolve(coupons);
+  //   });
+  // }
 
   public static async create(coupon: CouponCreatePayload) {
     const response = await axios.post(this.endpoint, coupon)
